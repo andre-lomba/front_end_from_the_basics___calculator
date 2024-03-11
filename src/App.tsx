7fabb1ef7d4bc72e7b4596d9d0d4daeecc4c2959
André Lomba <130799132+andre-lomba@users.noreply.github.com>
2024-03-11 17:19:39 -0300

    chore: eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { useEffect, useState } from 'react'
+
 function App() {
-  return <></>
+  const [count, setCount] = useState(1)
+
+  useEffect(() => {
+    console.log(count)
+  }, [count])
+
+  return <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
 }
 
 export default App
