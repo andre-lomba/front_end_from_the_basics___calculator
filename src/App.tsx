3def7b40e19e526bc0c3936eb0c539d5e18301ca
André Lomba <130799132+andre-lomba@users.noreply.github.com>
2024-03-13 18:02:26 -0300

    refactor: ThemeProvider wrapping Calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { useEffect, useState } from 'react'
+import Calculator from './components/calculator/Calculator'
+import ThemeProvider from './context/ThemeContext'
 
 function App() {
-  const [count, setCount] = useState(1)
-
-  useEffect(() => {
-    console.log(count)
-  }, [count])
-
-  return <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
+  return (
+    <ThemeProvider>
+      <Calculator />
+    </ThemeProvider>
+  )
 }
 
 export default App
