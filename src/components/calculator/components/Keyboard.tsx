c1c548f9a6b4becd7bc8ea4bcbaa47012e6f2a9c
André Lomba <130799132+andre-lomba@users.noreply.github.com>
2024-03-18 20:14:08 -0300

    feat: Keyboard component in grid style

--- src/components/calculator/components/Keyboard.tsx
+++ src/components/calculator/components/Keyboard.tsx
@@ -1,7 +1,85 @@
-type KeyboardProps = {
-  onChange: (value: string) => void
-}
+import { Moon, Sun } from 'lucide-react'
+import { useTheme } from '../../../hooks/useTheme'
+import Button from './Button'
+import './Keyboard.css'
 
-export default function Keyboard({ onChange }: KeyboardProps) {
-  return <div></div>
+export default function Keyboard() {
+  const { theme, toggleTheme } = useTheme()
+
+  return (
+    <div className="keyboard">
+      <div className="grid">
+        <Button
+          colored
+          size="regular"
+          data={{ label: 'C' }}
+          onClick={() => undefined}
+        />
+        <Button
+          size="regular"
+          data={{
+            label: theme === 'dark' ? <Sun size={40} /> : <Moon size={40} />,
+          }}
+          onClick={toggleTheme}
+        />
+        <Button size="regular" data={{ label: '%' }} onClick={console.log} />
+        <Button size="regular" data={{ label: '÷' }} onClick={console.log} />
+        <Button
+          size="regular"
+          data={{ value: '7', label: '7' }}
+          onClick={console.log}
+        />
+        <Button
+          size="regular"
+          data={{ value: '8', label: '8' }}
+          onClick={console.log}
+        />
+        <Button
+          size="regular"
+          data={{ value: '9', label: '9' }}
+          onClick={console.log}
+        />
+        <Button size="regular" data={{ label: '×' }} onClick={console.log} />
+        <Button
+          size="regular"
+          data={{ value: '4', label: '4' }}
+          onClick={console.log}
+        />
+        <Button
+          size="regular"
+          data={{ value: '5', label: '5' }}
+          onClick={console.log}
+        />
+        <Button
+          size="regular"
+          data={{ value: '6', label: '6' }}
+          onClick={console.log}
+        />
+        <Button size="regular" data={{ label: '−' }} onClick={console.log} />
+        <Button
+          size="regular"
+          data={{ value: '1', label: '1' }}
+          onClick={console.log}
+        />
+        <Button
+          size="regular"
+          data={{ value: '2', label: '2' }}
+          onClick={console.log}
+        />
+        <Button
+          size="regular"
+          data={{ value: '3', label: '3' }}
+          onClick={console.log}
+        />
+        <Button size="big" data={{ label: '+' }} onClick={console.log} />
+        <Button
+          size="regular"
+          data={{ value: '0', label: '0' }}
+          onClick={console.log}
+        />
+        <Button size="regular" data={{ label: '.' }} onClick={console.log} />
+        <Button size="regular" data={{ label: '=' }} onClick={console.log} />
+      </div>
+    </div>
+  )
 }
