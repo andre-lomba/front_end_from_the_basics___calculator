ba5c86b8afaf003503ada13cd31a396c55d81851
André Lomba <130799132+andre-lomba@users.noreply.github.com>
2024-03-20 16:30:09 -0300

    refactor: the main number will be no longer displayed as an input

--- src/components/calculator/components/Screen.tsx
+++ src/components/calculator/components/Screen.tsx
@@ -2,28 +2,24 @@ import './Screen.css'
 import { useTheme } from '../../../hooks/useTheme'
 import { isSymbol } from '../../../utils/functions/symbolsFn'
 
-type ScreenProps = React.InputHTMLAttributes<HTMLInputElement> & {
-  operation: string[]
-}
-
-export default function Screen({ operation, ...rest }: ScreenProps) {
+export default function Screen() {
   const { className } = useTheme()
   return (
     <div className={className('screen')}>
       <span className="operation">
-        {operation.map((op, index) => (
+        {[].map((op, index) => (
           <p
             key={index}
             style={op === '%' ? { fontSize: 13, fontWeight: 600 } : undefined}
             className={className(
-              `calculation ${isSymbol(op) ? 'especial' : ''}`,
+              `number-symbol ${isSymbol(op) ? ' especial' : ''}`,
             )}
           >
             {op}
           </p>
         ))}
       </span>
-      <input {...rest} type="text" className={className('input')} />
+      <span className={className('field')}>testa</span>
     </div>
   )
 }
