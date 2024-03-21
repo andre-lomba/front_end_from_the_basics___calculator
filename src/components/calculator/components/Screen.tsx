016a89dabaad11e3908e39ba97783d651af23083
André Lomba <130799132+andre-lomba@users.noreply.github.com>
2024-03-21 16:09:29 -0300

    refactor: adjustments in values and functions

--- src/components/calculator/components/Screen.tsx
+++ src/components/calculator/components/Screen.tsx
@@ -1,25 +1,27 @@
 import './Screen.css'
 import { useTheme } from '../../../hooks/useTheme'
-import { isSymbol } from '../../../utils/functions/symbolsFn'
+import { getOperator, isOperator } from '../../../utils/functions/operatorFn'
+import { useCalculator } from '../../../hooks/useCalculator'
 
 export default function Screen() {
+  const { operation } = useCalculator()
   const { className } = useTheme()
   return (
     <div className={className('screen')}>
       <span className="operation">
-        {[].map((op, index) => (
+        {operation.stagedOperation.map((op, index) => (
           <p
             key={index}
             style={op === '%' ? { fontSize: 13, fontWeight: 600 } : undefined}
             className={className(
-              `number-symbol ${isSymbol(op) ? ' especial' : ''}`,
+              `number-symbol ${isOperator(op) ? ' especial' : ''}`,
             )}
           >
-            {op}
+            {isOperator(op) ? getOperator(op) : op}
           </p>
         ))}
       </span>
-      <span className={className('field')}>testa</span>
+      <span className={className('field')}>{operation.mainNumber}</span>
     </div>
   )
 }
