1c44644e88c24c8e06aeed70c53628c850c91924
André Lomba <130799132+andre-lomba@users.noreply.github.com>
2024-03-21 16:14:32 -0300

    refactor: Button has type and behaviors associated with it

--- src/components/calculator/components/Button.tsx
+++ src/components/calculator/components/Button.tsx
@@ -1,44 +1,71 @@
-import { HTMLAttributes, ReactNode, useEffect } from 'react'
+import { useCallback, useEffect } from 'react'
 import { useTheme } from '../../../hooks/useTheme'
 
 import './Button.css'
+import { TNumber, Operator, TSymbol } from '../../../context/CalculatorContext'
+import { useCalculator } from '../../../hooks/useCalculator'
+import { getOperator } from '../../../utils/functions/operatorFn'
 
-type ButtonProps = HTMLAttributes<HTMLButtonElement> & {
-  data: { value?: string; label: string | ReactNode }
-  onClick: (value: string | ReactNode) => void
-  colored?: boolean
-  size: 'regular' | 'big'
-}
+type ButtonConditionalProps =
+  | { type: 'reset'; value: 'C' }
+  | { type: 'number'; value: TNumber }
+  | { type: 'operator'; value: Operator }
+  | { type: 'symbol'; value: TSymbol }
 
-export default function Button({
-  data,
-  onClick,
-  colored,
-  size,
-  ...props
-}: ButtonProps) {
+type ButtonProps = { size: 'regular' | 'big' } & ButtonConditionalProps
+
+export default function Button({ value, type, size }: ButtonProps) {
+  const { addOperator, addSymbol, addNumber, reset } = useCalculator()
   const { className } = useTheme()
+
+  const onClick = useCallback(() => {
+    switch (type) {
+      case 'reset':
+        reset()
+        break
+      case 'number':
+        addNumber(value)
+        break
+      case 'operator':
+        addOperator(value)
+        break
+      case 'symbol':
+        addSymbol(value)
+        break
+      default:
+        break
+    }
+  }, [addNumber, addOperator, addSymbol, reset, type, value])
 
   useEffect(() => {
     function handlePressKey(event: KeyboardEvent) {
-      if (event.key === data.value) {
-        onClick(data.value)
+      event.preventDefault()
+      if (type !== 'reset')
+        if (event.key.toLowerCase() === value.toLowerCase()) {
+          onClick()
+          return
+        }
+      if (event.key === 'Enter' && value === '=') {
+        onClick()
       }
     }
+
     document.addEventListener('keydown', handlePressKey)
 
     return () => {
       document.removeEventListener('keydown', handlePressKey)
     }
-  }, [onClick, data.value])
+  }, [onClick, type, value])
 
   return (
     <button
-      {...props}
-      onClick={() => onClick(data.value)}
-      className={className('button' + ' ' + size + (colored ? ' colored' : ''))}
+      onClick={onClick}
+      className={className(
+        'button' + ' ' + size + (type === 'reset' ? ' colored' : ''),
+      )}
+      autoFocus={value === '='}
     >
-      {data.label}
+      {type === 'operator' ? getOperator(value) : value}
     </button>
   )
 }
