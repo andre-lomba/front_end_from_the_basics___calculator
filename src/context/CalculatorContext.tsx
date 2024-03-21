b4368f69e8797d05fa75ebb1b50cf1ba3378f3c1
André Lomba <130799132+andre-lomba@users.noreply.github.com>
2024-03-21 16:08:00 -0300

    refactor: calculation logic

--- src/context/CalculatorContext.tsx
+++ src/context/CalculatorContext.tsx
@@ -1,16 +1,21 @@
-import { PropsWithChildren, createContext, useState } from 'react'
+import { PropsWithChildren, createContext, useEffect, useState } from 'react'
 
 type ClaculatorProviderProps = PropsWithChildren
 
-type Operation = { mainNumber: string; stagedOperation: string[] }
+export type TNumber = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
 
-type Operator = '+' | '-' | '*' | '/' | '%' | '='
+export type Operator = '+' | '-' | '*' | '/' | '%' | '='
 
-type TSymbol = '.' | '+/-'
+export type TSymbol = '.' | '+/-'
+
+type Operation = {
+  mainNumber: string
+  stagedOperation: string[]
+}
 
 type CalculatorContextProps = {
   operation: Operation
-  addValue: (value: string) => void
+  addNumber: (number: TNumber) => void
   addOperator: (operator: Operator) => void
   addSymbol: (symbol: TSymbol) => void
   reset: () => void
@@ -25,32 +30,127 @@ export default function ClaculatorProvider({
     mainNumber: '0',
     stagedOperation: [],
   })
+  // after an operation or an addition of an operator, the previous number remains in the main screen, but it is replaceble
+  const [isReplaceable, setIsReplaceable] = useState(false)
+  // after an equals operation, the result remains in the main screen, but as soon as a value is added, the calculator resets
+  const [isResetable, setIsResetable] = useState(false)
 
-  function addValue(value: string) {
-    setOperation((prev) => ({ ...prev, mainNumber: value }))
+  useEffect(() => {
+    function handlePressKey(event: KeyboardEvent) {
+      const number = operation.mainNumber
+      if (event.key === 'Backspace') {
+        if (isResetable)
+          setOperation((prev) => ({ ...prev, stagedOperation: [] }))
+        if (number === '0') return
+        if (
+          number.length === 1 ||
+          (number.length === 2 && number.includes('-'))
+        )
+          setOperation((prev) => ({ ...prev, mainNumber: '0' }))
+        else
+          setOperation((prev) => ({
+            ...prev,
+            mainNumber: prev.mainNumber.slice(0, -1),
+          }))
+      }
+    }
+
+    document.addEventListener('keydown', handlePressKey)
+
+    return () => {
+      document.removeEventListener('keydown', handlePressKey)
+    }
+  }, [operation.mainNumber])
+
+  function addNumber(number: TNumber) {
+    if (isResetable) {
+      setOperation({ mainNumber: number, stagedOperation: [] })
+      setIsResetable(false)
+      return
+    }
+
+    const mainNumber = isReplaceable
+      ? // main number is waiting to be replaced
+        number
+      : // if there's "." let it be a string, otherwise "5.0" would be converted to "5", making it impossible to write something like "5.01" etc
+      operation.mainNumber.includes('.')
+      ? operation.mainNumber + number
+      : // prevent number from being "05"
+        parseFloat(operation.mainNumber + number).toString()
+
+    setOperation((prev) => ({
+      ...prev,
+      mainNumber,
+    }))
+    setIsReplaceable(false)
   }
 
   function addOperator(operator: Operator) {
+    if (isResetable) {
+      if (operator !== '=') {
+        setOperation((prev) => ({
+          ...prev,
+          stagedOperation: [prev.mainNumber, operator],
+        }))
+        setIsReplaceable(true)
+        setIsResetable(false)
+      }
+      return
+    }
+
+    const staged = operation.stagedOperation.length > 1
+
+    if (staged) {
+      equals(operator)
+      return
+    }
+
+    if (operator === '=') return
+
+    const trimmedNumber = parseFloat(operation.mainNumber).toString()
+
     setOperation((prev) => ({
       ...prev,
-      stagedOperation: [...prev.stagedOperation, prev.mainNumber, operator],
+      mainNumber: trimmedNumber,
+      stagedOperation: [trimmedNumber, operator],
     }))
+    setIsReplaceable(true)
   }
 
   function addSymbol(symbol: TSymbol) {
     if (symbol === '+/-') {
       const minusIndex = operation.mainNumber.indexOf('-')
-      if (minusIndex < 0) {
+      if (
+        minusIndex < 0 &&
+        operation.mainNumber !== '0' &&
+        operation.mainNumber !== '0.'
+      ) {
         const newNumber = '-' + operation.mainNumber
-        setOperation((prev) => ({ ...prev, mainNumber: newNumber }))
+        const stagedOperation = isResetable ? [] : operation.stagedOperation
+        setOperation({ stagedOperation, mainNumber: newNumber })
+        setIsResetable(false)
       } else if (minusIndex === 0) {
         const newNumber = operation.mainNumber.slice(1)
-        setOperation((prev) => ({ ...prev, mainNumber: newNumber }))
+        const stagedOperation = isResetable ? [] : operation.stagedOperation
+        setOperation({ stagedOperation, mainNumber: newNumber })
+        setIsResetable(false)
       }
       return
     }
 
     if (symbol === '.') {
+      if (isResetable) {
+        setOperation({ mainNumber: '0.', stagedOperation: [] })
+        setIsResetable(false)
+        setIsReplaceable(false)
+        return
+      }
+      if (isReplaceable) {
+        setOperation((prev) => ({ ...prev, mainNumber: '0.' }))
+        setIsReplaceable(false)
+        return
+      }
+
       if (operation.mainNumber.includes(symbol)) return
       const newNumber = operation.mainNumber + '.'
       setOperation((prev) => ({ ...prev, mainNumber: newNumber }))
@@ -58,15 +158,63 @@ export default function ClaculatorProvider({
   }
 
   function reset() {
-    setOperation({
-      mainNumber: '0',
-      stagedOperation: [],
-    })
+    setOperation({ mainNumber: '0', stagedOperation: [] })
+  }
+
+  function equals(operator: Operator) {
+    const staged = operation.stagedOperation[0]
+    const op = operation.stagedOperation[1]
+    const number = operation.mainNumber
+
+    if (!!staged && !!op) {
+      let result: number
+
+      switch (op) {
+        case '+':
+          result = parseFloat(staged) + parseFloat(number)
+          break
+        case '-':
+          result = parseFloat(staged) - parseFloat(number)
+          break
+        case '*':
+          result = parseFloat(staged) * parseFloat(number)
+          break
+        case '%':
+          result = parseFloat(staged) % parseFloat(number)
+          break
+        case '/':
+          result = parseFloat(staged) / parseFloat(number)
+          break
+        default:
+          result = 0
+          break
+      }
+
+      result = parseFloat(result.toFixed(8))
+
+      if (operator === '=') {
+        const trimmedNumber = parseFloat(number).toString()
+        const fomattedNumber = trimmedNumber.includes('-')
+          ? `(${trimmedNumber})`
+          : trimmedNumber
+        setOperation({
+          stagedOperation: [staged, op, fomattedNumber, '='],
+          mainNumber: result.toString(),
+        })
+        setIsResetable(true)
+      } else {
+        setOperation({
+          stagedOperation: [result.toString(), operator],
+          mainNumber: result.toString(),
+        })
+        setIsReplaceable(true)
+      }
+    }
   }
 
   return (
     <CalculatorContext.Provider
-      value={{ operation, addOperator, addSymbol, addValue, reset }}
+      value={{ operation, addOperator, addSymbol, addNumber, reset }}
     >
       {children}
     </CalculatorContext.Provider>
